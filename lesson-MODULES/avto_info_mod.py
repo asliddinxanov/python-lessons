1a0a8a606424f950527c22ed643afd2ca8cea9da
Asliddin <100646459+asliddinxanov@users.noreply.github.com>
2022-04-17 10:05:53 +0900

    update

--- lesson-MODULES/avto_info_mod.py
+++ lesson-MODULES/avto_info_mod.py
@@ -5,32 +5,32 @@ Created on Sat Mar 19 09:08:28 2022
 @author: asliddinxanov
 """
 
-def avto_info(kompaniya, modeli, color, yil=None, price=None):
-    avto = {
-        'kompaniya': kompaniya,
-        'model': modeli,
-        'rang': color,
-        'yili': yil,
-        'narxi': price}
+def avto_info(komp,model,color,kp,year,price=None):
+    avto = {'komp' : komp,
+            'model' : model,
+            'color' : color,
+            'kp' : kp,
+            'year' : year,
+            'price' : price}
     return avto
 
-# def avto_enter():
-#     """avto malumot kiritish"""
-#     avtos = []
-#     while True:
-#         print("Quyidagi malumotlarni kiriting.")
-#         kompaniya = input("Iishlab chiqaruvchi: ")
-#         modeli = input("Madeli: ")
-#         color = input("Rangi: ")
-#         yil = int(input("Yili: "))
-#         price = int(input("Narxi: "))
-#         avtos.append(avto_info(kompaniya, modeli, color, yil, price))
-#         j = input("Yana go'shasizmi?(yes/no)")
-#         if j == "no":
-#             break
-#     return avtos
+def avto_enter():
+    """avto malumot kiritish"""
+    avtos = []
+    while True:
+        print("Quyidagi malumotlarni kiriting.")
+        kompaniya = input("Iishlab chiqaruvchi: ")
+        modeli = input("Madeli: ")
+        color = input("Rangi: ")
+        yil = int(input("Yili: "))
+        price = int(input("Narxi: "))
+        avtos.append(avto_info(kompaniya, modeli, color, yil, price))
+        j = input("Yana go'shasizmi?(yes/no)")
+        if j == "no":
+            break
+    return avtos
 
 def info_print(avto_info):
-    print(f"{avto_info['kompaniya'].title()}, {avto_info['model'].title()}, "
-          f"{avto_info['yili']}-yil, rangi {avto_info['rang']},"
-          f" narxi-{avto_info['narxi']}$.")
+    print(f"{avto_info['color'].title()}, {avto_info['komp'].upper()}, "
+          f"{avto_info['model'].upper()}, {avto_info['kp']} karobka, "
+          f"{avto_info['year']} - yil, {avto_info['price']}$.")
--- lesson-MODULES/main.py
+++ lesson-MODULES/main.py
@@ -7,6 +7,6 @@ Created on Sat Mar 19 09:07:42 2022
 
 from avto_info_mod import avto_info, info_print
 
-avto = avto_info("GM", "Malibu", "qora", 2020,3500)
+avto = avto_info("GM", "Malibu", "qora", "avto", 2020, 35000)
 info_print(avto)
 
